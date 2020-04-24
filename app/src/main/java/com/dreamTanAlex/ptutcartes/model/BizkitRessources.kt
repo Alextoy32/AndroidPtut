64d6302e1dc84b693631e2375ba88ab3e571b405
BuildTools <unconfigured@null.spigotmc.org>
2020-04-24 09:04:26 +0200

    bizkit en cours changing name not work

---
+++ app/src/main/java/com/dreamTanAlex/ptutcartes/model/BizkitRessources.kt
@@ -0,0 +1,11 @@
+package com.dreamTanAlex.ptutcartes.model
+
+/*
+Created by Alexis Boutan on 21/04/2020 from the project : PtutCartes
+*/
+
+class BizkitRessources(
+    var rule: Int,
+    var dice1: Int,
+    var dice2: Int
+)
--- app/src/main/java/com/dreamTanAlex/ptutcartes/ui/bizkit/BizkitViewModel.kt
+++ app/src/main/java/com/dreamTanAlex/ptutcartes/ui/bizkit/BizkitViewModel.kt
@@ -2,14 +2,77 @@ package com.dreamTanAlex.ptutcartes.ui.bizkit
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.dreamTanAlex.ptutcartes.R
+import com.dreamTanAlex.ptutcartes.model.BizkitParty
+import com.dreamTanAlex.ptutcartes.model.BizkitRessources
+import com.dreamTanAlex.ptutcartes.model.Player
+import kotlin.random.Random
 
 class BizkitViewModel : ViewModel() {
 
-    data class gameString(var rule: MutableLiveData<String>, var dice1: String, var dice2: String)
+    var party = BizkitParty()
 
-    var displayed = gameString(MutableLiveData("startregle"), "ic_dice_one", "ic_dice_one")
+    var dice1 = 1
+    var dice2 = 1
+
+    var displayed = MutableLiveData(
+        BizkitRessources(
+            R.string.startRule,
+            R.drawable.ic_dice_one,
+            R.drawable.ic_dice_one
+        )
+    )
+
+    private var drawableDice1 = 0
+    private var drawableDice2 = 0
+    private var stringRule = 0
+
+    init {
+        party.players.add(Player("Tan", false, 0))
+        party.players.add(Player("Alexis", false, 0))
+        party.players.add(Player("Maxime", false, 0))
+        party.startParty()
+    }
 
     fun rollDice() {
-        displayed.rule.postValue("Ceci est la regle")
+        party.nextTurn()
+        dice1 = Random.nextInt(1, 7)
+        dice2 = Random.nextInt(1, 7)
+        drawableDice1 = diceToDrawable(dice1)
+        drawableDice2 = diceToDrawable(dice2)
+
+        when {
+            dice1 == dice2 && dice1 == 1 -> stringRule = R.string.double1
+            dice1 == dice2 && dice1 == 5 -> stringRule = R.string.double5
+            dice1 == dice2 -> stringRule = R.string.doubleN
+            dice1 + dice2 == 3 -> pigeon()
+            dice1 + dice2 == 5 -> stringRule = R.string.som5
+            dice1 + dice2 == 7 -> stringRule = R.string.som7
+            dice1 + dice2 == 9 -> stringRule = R.string.som9
+            dice1 + dice2 == 10 -> stringRule = R.string.som10
+            dice1 + dice2 == 11 -> stringRule = R.string.som11
+            else -> stringRule = R.string.passe
+        }
+
+        displayed.postValue(BizkitRessources(stringRule, drawableDice1, drawableDice2))
     }
+
+    private fun diceToDrawable(dice: Int): Int {
+        var d = 0
+        when (dice) {
+            1 -> d = R.drawable.ic_dice_one
+            2 -> d = R.drawable.ic_dice_two
+            3 -> d = R.drawable.ic_dice_three
+            4 -> d = R.drawable.ic_dice_four
+            5 -> d = R.drawable.ic_dice_five
+            6 -> d = R.drawable.ic_dice_six
+        }
+        return d
+    }
+
+    private fun pigeon() {
+        stringRule = R.string.newPigeon
+    }
+
+
 }
